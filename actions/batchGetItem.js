18b0914e4bd26afb5a68d59aa40d6dbb39cfa83c
Michael Hart <michael.hart.au@gmail.com>
2013-10-23 21:38:03 +1100

    Clean up batch methods using async

--- actions/batchGetItem.js
+++ actions/batchGetItem.js
@@ -1,32 +1,47 @@
-var getItem = require('./getItem'),
+var async = require('async'),
+    getItem = require('./getItem'),
     db = require('../db')
 
 module.exports = function batchGetItem(data, cb) {
-  var remaining = 0, responses = {}, table, seenKeys, i, key, keyStr
-  for (table in data.RequestItems) {
-    responses[table] = []
-    seenKeys = {}
-    for (i = 0; i < data.RequestItems[table].Keys.length; i++) {
-      key = data.RequestItems[table].Keys[i]
-      keyStr = JSON.stringify(key)
-      if (seenKeys[keyStr])
-        return cb(db.validationError('Provided list of item keys contains duplicates'))
-      seenKeys[keyStr] = true
-    }
-    data.RequestItems[table].Keys.forEach(function(key) {
-      var options = {TableName: table, Key: key}
-      if (data.RequestItems[table].AttributesToGet)
-        options.AttributesToGet = data.RequestItems[table].AttributesToGet
-      getItem(options, checkDone(table))
-      remaining++
-    })
-  }
-  function checkDone(table) {
-    return function(err, item) {
+  var requests = {}
+
+  async.series([
+    async.each.bind(async, Object.keys(data.RequestItems), addTableRequests),
+    async.parallel.bind(async, requests),
+  ], function(err, responses) {
+    if (err) return cb(err)
+    cb(null, {Responses: responses[1], UnprocessedKeys: {}})
+  })
+
+  function addTableRequests(tableName, cb) {
+    db.getTable(tableName, function(err, table) {
       if (err) return cb(err)
-      if (item.Item) responses[table].push(item.Item)
-      if (!--remaining) return cb(null, {Responses: responses, UnprocessedKeys: {}})
-    }
+
+      var req = data.RequestItems[tableName], i, key, options, gets = [], seenKeys = {}
+
+      for (i = 0; i < req.Keys.length; i++) {
+        key = req.Keys[i]
+
+        options = {TableName: tableName, Key: key}
+        if (req.AttributesToGet) options.AttributesToGet = req.AttributesToGet
+        gets.push(options)
+
+        key = db.validateKey(key, table)
+        if (key instanceof Error) return cb(key)
+        if (seenKeys[key])
+          return cb(db.validationError('Provided list of item keys contains duplicates'))
+        seenKeys[key] = true
+      }
+
+      requests[tableName] = function(cb) {
+        async.map(gets, getItem, function(err, resps) {
+          if (err) return cb(err)
+          cb(null, resps.map(function(res) { return res.Item }).filter(function(x) { return x }))
+        })
+      }
+
+      cb()
+    })
   }
 }
 
--- actions/batchWriteItem.js
+++ actions/batchWriteItem.js
@@ -4,49 +4,45 @@ var async = require('async'),
     db = require('../db')
 
 module.exports = function batchWriteItem(data, cb) {
-  var puts = [], deletes = []
+  var actions = []
 
-  function validateTableKeys(tableName, cb) {
+  async.series([
+    async.each.bind(async, Object.keys(data.RequestItems), addTableActions),
+    async.parallel.bind(async, actions),
+  ], function(err) {
+    if (err) return cb(err)
+    cb(null, {UnprocessedItems: {}})
+  })
+
+  function addTableActions(tableName, cb) {
     db.getTable(tableName, function(err, table) {
       if (err) return cb(err)
 
       var reqs = data.RequestItems[tableName], i, req, key, seenKeys = {}
+
       for (i = 0; i < reqs.length; i++) {
         req = reqs[i]
+
         if (req.PutRequest) {
+
+          actions.push(putItem.bind(null, {TableName: tableName, Item: req.PutRequest.Item}))
+
           key = db.validateItem(req.PutRequest.Item, table)
 
-          if (key instanceof Error) return cb(key)
-          if (seenKeys[key])
-            return cb(db.validationError('Provided list of item keys contains duplicates'))
-          seenKeys[key] = true
-
-          puts.push({TableName: tableName, Item: req.PutRequest.Item})
         } else if (req.DeleteRequest) {
+
+          actions.push(deleteItem.bind(null, {TableName: tableName, Key: req.DeleteRequest.Key}))
+
           key = db.validateKey(req.DeleteRequest.Key, table)
-
-          if (key instanceof Error) return cb(key)
-          if (seenKeys[key])
-            return cb(db.validationError('Provided list of item keys contains duplicates'))
-          seenKeys[key] = true
-
-          deletes.push({TableName: tableName, Key: req.DeleteRequest.Key})
         }
+        if (key instanceof Error) return cb(key)
+        if (seenKeys[key])
+          return cb(db.validationError('Provided list of item keys contains duplicates'))
+        seenKeys[key] = true
       }
 
       cb()
     })
   }
-
-  async.series([
-    async.apply(async.forEach, Object.keys(data.RequestItems), validateTableKeys),
-    async.apply(async.parallel, [
-      async.apply(async.forEach, puts, putItem),
-      async.apply(async.forEach, deletes, deleteItem),
-    ])
-  ], function(err) {
-    if (err) return cb(err)
-    cb(null, {UnprocessedItems: {}})
-  })
 }
 
