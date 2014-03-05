4496372f8fcd2d300f3b32d997a43ed7e062919f
Michael Hart <michael.hart.au@gmail.com>
2014-03-05 15:43:06 +1100

    Added the global secondary index throughput updates

--- actions/updateTable.js
+++ actions/updateTable.js
@@ -10,32 +10,50 @@ module.exports = function updateTable(store, data, cb) {
     tableDb.get(key, function(err, table) {
       if (err) return cb(err)
 
-      var readDiff = data.ProvisionedThroughput.ReadCapacityUnits - table.ProvisionedThroughput.ReadCapacityUnits,
-          writeDiff = data.ProvisionedThroughput.WriteCapacityUnits - table.ProvisionedThroughput.WriteCapacityUnits
+      var updates = getThroughputUpdates(data, table),
+          i, update, dataThroughput, tableThroughput, readDiff, writeDiff
 
-      if (!readDiff && !writeDiff)
-        return cb(db.validationError(
-          'The provisioned throughput for the table will not change. The requested value equals the current value. ' +
-          'Current ReadCapacityUnits provisioned for the table: ' + table.ProvisionedThroughput.ReadCapacityUnits +
-          '. Requested ReadCapacityUnits: ' + data.ProvisionedThroughput.ReadCapacityUnits + '. ' +
-          'Current WriteCapacityUnits provisioned for the table: ' + table.ProvisionedThroughput.WriteCapacityUnits +
-          '. Requested WriteCapacityUnits: ' + data.ProvisionedThroughput.WriteCapacityUnits + '. ' +
-          'Refer to the Amazon DynamoDB Developer Guide for current limits and how to request higher limits.'))
+      for (i = 0; i < updates.length; i++) {
+        update = updates[i]
+        dataThroughput = update.dataThroughput
+        tableThroughput = update.tableThroughput
+        readDiff = dataThroughput.ReadCapacityUnits - tableThroughput.ReadCapacityUnits
+        writeDiff = dataThroughput.WriteCapacityUnits - tableThroughput.WriteCapacityUnits
 
-      table.TableStatus = 'UPDATING'
-      if (readDiff > 0 || writeDiff > 0) table.ProvisionedThroughput.LastIncreaseDateTime = Date.now() / 1000
-      if (readDiff < 0 || writeDiff < 0) table.ProvisionedThroughput.LastDecreaseDateTime = Date.now() / 1000
+        if (!readDiff && !writeDiff)
+          return cb(db.validationError(
+            'The provisioned throughput for the table will not change. The requested value equals the current value. ' +
+            'Current ReadCapacityUnits provisioned for the table: ' + tableThroughput.ReadCapacityUnits +
+            '. Requested ReadCapacityUnits: ' + dataThroughput.ReadCapacityUnits + '. ' +
+            'Current WriteCapacityUnits provisioned for the table: ' + tableThroughput.WriteCapacityUnits +
+            '. Requested WriteCapacityUnits: ' + dataThroughput.WriteCapacityUnits + '. ' +
+            'Refer to the Amazon DynamoDB Developer Guide for current limits and how to request higher limits.'))
+
+        update.setStatus('UPDATING')
+
+        if (readDiff > 0 || writeDiff > 0) tableThroughput.LastIncreaseDateTime = Date.now() / 1000
+        if (readDiff < 0 || writeDiff < 0) tableThroughput.LastDecreaseDateTime = Date.now() / 1000
+
+        update.readDiff = readDiff
+        update.writeDiff = writeDiff
+      }
 
       tableDb.put(key, table, function(err) {
         if (err) return cb(err)
 
         setTimeout(function() {
 
-          // Shouldn't need to lock/fetch as nothing should have changed
-          table.TableStatus = 'ACTIVE'
-          if (readDiff < 0 || writeDiff < 0) table.ProvisionedThroughput.NumberOfDecreasesToday++
-          table.ProvisionedThroughput.ReadCapacityUnits = data.ProvisionedThroughput.ReadCapacityUnits
-          table.ProvisionedThroughput.WriteCapacityUnits = data.ProvisionedThroughput.WriteCapacityUnits
+          updates.forEach(function(update) {
+            var dataThroughput = update.dataThroughput, tableThroughput = update.tableThroughput
+
+            // Shouldn't need to lock/fetch as nothing should have changed
+            update.setStatus('ACTIVE')
+
+            if (update.readDiff < 0 || update.writeDiff < 0) tableThroughput.NumberOfDecreasesToday++
+
+            tableThroughput.ReadCapacityUnits = dataThroughput.ReadCapacityUnits
+            tableThroughput.WriteCapacityUnits = dataThroughput.WriteCapacityUnits
+          })
 
           tableDb.put(key, table, function(err) {
             // TODO: Need to check this
@@ -51,4 +69,27 @@ module.exports = function updateTable(store, data, cb) {
 
 }
 
-
+function getThroughputUpdates(data, table) {
+  var updates = []
+  if (data.ProvisionedThroughput) {
+    updates.push({
+      dataThroughput: data.ProvisionedThroughput,
+      tableThroughput: table.ProvisionedThroughput,
+      setStatus: function(status) { table.TableStatus = status },
+    })
+  }
+  if (data.GlobalSecondaryIndexUpdates) {
+    data.GlobalSecondaryIndexUpdates.forEach(function(update) {
+      table.GlobalSecondaryIndexes.forEach(function(index) {
+        if (index.IndexName == update.Update.IndexName) {
+          updates.push({
+            dataThroughput: update.Update.ProvisionedThroughput,
+            tableThroughput: index.ProvisionedThroughput,
+            setStatus: function(status) { index.IndexStatus = status },
+          })
+        }
+      })
+    })
+  }
+  return updates
+}
