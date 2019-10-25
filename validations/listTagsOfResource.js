54f0b4c896314ad977f97ee3fa6373ed42646818
Ahmed Salama <asalama@lyft.com>
2019-10-25 16:18:35 -0700

    modify the table arn regex

--- validations/listTagsOfResource.js
+++ validations/listTagsOfResource.js
@@ -3,6 +3,6 @@ exports.types = {
     type: 'String',
     required: true,
     tableName: false,
-    regex: 'arn:(aws[a-zA-Z-]*)?:dynamodb:[a-z]{2}((-gov)|(-iso(b?)))?-[a-z]+-\\d{1}:\\d{12}:table:[a-zA-Z0-9-_]+(:(\\$LATEST|[a-zA-Z0-9-_]+))?',
+    regex: 'arn:aws:dynamodb:(.+):(.+):table/(.+)',
   },
 }
